88f33841308f7ac1a4f6044fffb469ae2f3c0f93
Simon Daniel <h51356@stud.u-szeged.hu>
2023-02-22 21:51:40 +0100

    Base angular project and login,register,main page component generated

---
+++ web_client/milliomos/src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { MainComponent } from './main/main.component';
+import { RegistrationComponent } from './registration/registration.component';
+
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'registration',
+    component: RegistrationComponent
+  },
+  {
+    path: 'main',
+    component: MainComponent
+  },
+  {
+    path: '',
+    redirectTo: 'main',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
