a48a6510fcd0341c2bf98a1a5cc10f3fd45921fe
Csizi Gergő Lajos <h043778@stud.u-szeged.hu>
2023-03-05 11:32:20 +0000

    Add more fields for user in the database

--- api/src/models/user.ts
+++ api/src/models/user.ts
@@ -1,16 +1,32 @@
-import { DataTypes, Model, InferAttributes, InferCreationAttributes  } from 'sequelize'
-import { sequelize } from '../db/sequelizeConnector'
+import {DataTypes, Model, InferAttributes, InferCreationAttributes} from 'sequelize'
+import {sequelize} from '../db/sequelizeConnector'
 
-class User extends Model<InferAttributes<User>,InferCreationAttributes<User>> {
+class User extends Model<InferAttributes<User>, InferCreationAttributes<User>> {
 
     declare tokenKey: string
+    declare name: string
+    declare email: string
+    declare firstName: string
+    declare lastName: string
     declare isAdmin: boolean
 }
 
 User.init({
         tokenKey: {
-            type: new DataTypes.STRING(255),
+            type: new DataTypes.STRING(28),
             primaryKey: true
+        },
+        name: {
+            type: new DataTypes.STRING(255),
+        },
+        email: {
+            type: new DataTypes.STRING(256), //https://www.rfc-editor.org/rfc/rfc5321#section-4.5.3
+        },
+        firstName: {
+            type: new DataTypes.STRING(255),
+        },
+        lastName: {
+            type: new DataTypes.STRING(255),
         },
         isAdmin: {
             type: new DataTypes.BOOLEAN
