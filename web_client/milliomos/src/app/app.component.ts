a2d093d105acd84dcf7f4e87e1917211c9cd0d75
Gergo Csizi <gergocs@inf.u-szeged.hu>
2023-03-28 09:43:05 +0200

    Add cookies

--- web_client/milliomos/src/app/app.component.ts
+++ web_client/milliomos/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { Router } from '@angular/router';
 import firebase from "firebase/compat/app"
 import { environment } from 'src/environments/environment';
@@ -8,10 +8,32 @@ import { environment } from 'src/environments/environment';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'milliomos';
 
   constructor() {
     firebase.initializeApp(environment.firebase);
+  }
+
+  ngOnInit() {
+    let cc = window as any;
+    cc.cookieconsent.initialise({
+      palette: {
+        popup: {
+          background: "#164969" //TODO: Colors
+        },
+        button: {
+          background: "#ffe000",
+          text: "#164969"
+        }
+      },
+      theme: "classic",
+      content: {
+        message: "Az oldal nagyon finom sütiket használ",
+        dismiss: "Rendben",
+        link: "Több információ",
+        href: "/dataprivacy"
+      }
+    });
   }
 }
