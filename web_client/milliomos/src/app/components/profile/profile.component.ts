fb403b3ac6891dc4c8a149adefd83cc667002b5a
Gergo Csizi <gergocs@inf.u-szeged.hu>
2023-05-08 12:40:02 +0200

    Fix display names of achievements

--- web_client/milliomos/src/app/components/profile/profile.component.ts
+++ web_client/milliomos/src/app/components/profile/profile.component.ts
@@ -1,11 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, FormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
-import { UserModell } from '../../models/usermodell';
-import { AuthService } from '../../services/auth.service';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
+import {Router} from '@angular/router';
+import {UserModell} from '../../models/usermodell';
+import {AuthService} from '../../services/auth.service';
 import {Score} from "../../models/score";
 import {ScoreService} from "../../services/score.service";
-import {KeyValue} from "@angular/common";
 
 @Component({
   selector: 'app-profile',
@@ -32,7 +31,7 @@ export class ProfileComponent implements OnInit {
 
   constructor(public router: Router, private auth: AuthService, private scoreService: ScoreService) {
     let userdatas = window.localStorage.getItem("userdatas")
-    if (userdatas){
+    if (userdatas) {
       this.userdata = JSON.parse(userdatas)
       let userdata = JSON.parse(userdatas)
       this.updateForm.get('username')?.setValue(decodeURIComponent(userdata.name))
@@ -73,15 +72,15 @@ export class ProfileComponent implements OnInit {
           // @ts-ignore
           if (entry[1] != "" && entry[1] != undefined && entry[1] != null) {
             entry[1].forEach((value: string) => {
-              this.achievements.set(decodeURIComponent(value), "assets/images/achievements/" + decodeURIComponent(value) + ".svg")
+              this.achievements.set(this.valueToHungarianValue(decodeURIComponent(value)), "assets/images/achievements/" + decodeURIComponent(value) + ".svg")
               counter++;
 
-              if (counter >= 10){
+              if (counter >= 10) {
                 return;
               }
             })
 
-            if (counter >= 10){
+            if (counter >= 10) {
               return;
             }
           }
@@ -101,13 +100,13 @@ export class ProfileComponent implements OnInit {
     this.skipIntroChecked = event.target.checked;
   }
 
-  redirectToLobby(){
+  redirectToLobby() {
     this.router.navigate(['/lobby']);
   }
 
-  async updateuser(){
+  async updateuser() {
     if (!this.userdata)
-    return
+      return
     this.userdata.name = this.updateForm.get("username")?.value
     this.userdata.firstName = this.updateForm.get("firstName")?.value
     this.userdata.lastName = this.updateForm.get("lastName")?.value
@@ -121,10 +120,10 @@ export class ProfileComponent implements OnInit {
       } else {
         this.introSkip = false;
       }
-      window.localStorage.setItem('introSkipped',JSON.stringify(this.introSkip))
+      window.localStorage.setItem('introSkipped', JSON.stringify(this.introSkip))
 
       this.loading = false
-    }).catch(err =>{
+    }).catch(err => {
       this.loading = false
     })
   }
@@ -134,14 +133,38 @@ export class ProfileComponent implements OnInit {
   }
 
   timeToString(time: bigint) {
-    let nTime = Number(time)/1000
-    if (nTime > 60){
-      let perc = Math.floor(nTime/60)
-      let mp = Math.round(nTime - perc*60)
+    let nTime = Number(time) / 1000
+    if (nTime > 60) {
+      let perc = Math.floor(nTime / 60)
+      let mp = Math.round(nTime - perc * 60)
       return (perc.toString() + " perc " + mp.toString() + " másodperc")
-    }else{
+    } else {
       return Math.round(nTime).toString() + " másodperc"
     }
+  }
+
+  valueToHungarianValue(value: string): string {
+    if (value.includes('kredit')) {
+      return value;
+    }
+
+    if (value.includes('lvl')) {
+      return value.substring(0, value.length - 4) + ' ' + value.substring(value.length - 1, value.length) + ' szint';
+    }
+
+    if (value.includes('win')){
+      return value.replace('win', 'győzelem');
+    }
+
+    if (value.includes('Games')) {
+      return value.replace('Games', 'játék');
+    }
+
+    if (value.includes('level')) {
+      return value.replace('level', '') + ' szint mester';
+    }
+
+    return value;
   }
 
   protected readonly decodeURI = decodeURI;
